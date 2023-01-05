71114050e23a5d73bec32f386628287b1d268d51
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2023-01-05 11:03:49 -0800

    feat: define proto of resource mapping (#5)
    squash and signed commit

---
+++ apis/v1alpha1/v1alpha1.go
@@ -0,0 +1,18 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate protoc -I../../protos/v1alpha1 --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative resource_mapping.proto
+
+// Package v1alpha1 contains versioned pmap contracts, e.g. resource mapping definition.
+package v1alpha1
