8bb7793fc4993e38cbfc88ca589d3b4e1421da40
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2023-01-24 10:32:45 -0800

    refactor: use protoutil in pkg (#11)
    * use protoutil in pkg
    
    * fixed lint

--- pkg/mapping/processors/assetinventory.go
+++ pkg/mapping/processors/assetinventory.go
@@ -17,13 +17,13 @@ package processors
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
 
 	"cloud.google.com/go/asset/apiv1/assetpb"
 	"github.com/abcxyz/pkg/logging"
+	"github.com/abcxyz/pkg/protoutil"
 	"github.com/abcxyz/pmap/apis/v1alpha1"
 	"google.golang.org/api/iterator"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -181,7 +181,7 @@ func (p *AssetInventoryProcessor) validateAndEnrich(ctx context.Context, resourc
 		assetInventoryAnnos["iamPolicies"] = iamPolicies
 	}
 
-	assetInventorySpb, err := toProtoStruct(assetInventoryAnnos)
+	assetInventorySpb, err := protoutil.ToProtoStruct(assetInventoryAnnos)
 	if err != nil {
 		return nil, fmt.Errorf("failed to convert Asset Inventory annotations to structpb.Struct: %w", err)
 	}
@@ -237,20 +237,6 @@ func (p *AssetInventoryProcessor) Stop() error {
 		return fmt.Errorf("failed to stop Asset Inventory Processor: %w", err)
 	}
 	return nil
-}
-
-// toProtoStruct converts v, which must marshal into a JSON object,
-// into a proto struct.
-func toProtoStruct(v any) (*structpb.Struct, error) {
-	jb, err := json.Marshal(v)
-	if err != nil {
-		return nil, fmt.Errorf("json.Marshal: %w", err)
-	}
-	x := &structpb.Struct{}
-	if err := x.UnmarshalJSON(jb); err != nil {
-		return nil, fmt.Errorf("structpb.Struct.UnmarshalJSON: %w", err)
-	}
-	return x, nil
 }
 
 // mergeAnnotations merges two annotations represented by structpb.Struct,
