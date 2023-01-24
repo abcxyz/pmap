f1b0e30bccd263044dfa6437dc83f018dffd7134
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2023-01-24 09:54:11 -0800

    added CAIS processor and enbled go unit test in CI/CD

--- go.mod
+++ go.mod
@@ -2,4 +2,45 @@ module github.com/abcxyz/pmap
 
 go 1.19
 
-require google.golang.org/protobuf v1.28.1
+require (
+	cloud.google.com/go/asset v1.11.0
+	github.com/abcxyz/pkg v0.1.1
+	github.com/google/go-cmp v0.5.9
+	google.golang.org/api v0.106.0
+	google.golang.org/genproto v0.0.0-20230106154932-a12b697841d9
+	google.golang.org/grpc v1.51.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	cloud.google.com/go v0.105.0 // indirect
+	cloud.google.com/go/accesscontextmanager v1.4.0 // indirect
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v0.8.0 // indirect
+	cloud.google.com/go/longrunning v0.3.0 // indirect
+	cloud.google.com/go/osconfig v1.10.0 // indirect
+	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.6 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ pkg/mapping/processors/assetinventory_test.go
@@ -0,0 +1,308 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processors
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"cloud.google.com/go/asset/apiv1/assetpb"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/abcxyz/pmap/apis/v1alpha1"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/structpb"
+
+	asset "cloud.google.com/go/asset/apiv1"
+	v1 "google.golang.org/genproto/googleapis/iam/v1" //nolint:staticcheck // "cloud.google.com/go/asset/apiv1" still uses v1.Policy(deprecated).
+)
+
+func TestParseProject(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name          string
+		resourceName  string
+		wantProject   string
+		wantErrSubstr string
+	}{
+		{
+			name:         "success",
+			resourceName: "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+			wantProject:  "projects/test-project",
+		},
+		{
+			name:         "failure_with_no_project_exists_in_resource_name",
+			resourceName: "//storage.googleapis.com/test-bucket",
+			wantProject:  "",
+		},
+		{
+			name:          "failure_with_invalid_resource_name",
+			resourceName:  "//pubsub.googleapis.com/projects//test-project/topics/test-topic",
+			wantProject:   "",
+			wantErrSubstr: "invalid resource name: //pubsub.googleapis.com/projects//test-project/topics/test-topic",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotProject, gotErr := parseProject(tc.resourceName)
+			if diff := testutil.DiffErrString(gotErr, tc.wantErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+			if diff := cmp.Diff(tc.wantProject, gotProject); diff != "" {
+				t.Errorf("ParseProject(%+v) got diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
+
+type fakeAssetInventoryServer struct {
+	assetpb.UnimplementedAssetServiceServer
+
+	searchAllResourcesData   *assetpb.SearchAllResourcesResponse
+	searchAllIamPoliciesData *assetpb.SearchAllIamPoliciesResponse
+	searchAllResourcesErr    error
+	searchAllIamPoliciesErr  error
+}
+
+func (s *fakeAssetInventoryServer) SearchAllResources(context.Context, *assetpb.SearchAllResourcesRequest) (*assetpb.SearchAllResourcesResponse, error) {
+	return s.searchAllResourcesData, s.searchAllResourcesErr
+}
+
+func (s *fakeAssetInventoryServer) SearchAllIamPolicies(context.Context, *assetpb.SearchAllIamPoliciesRequest) (*assetpb.SearchAllIamPoliciesResponse, error) {
+	return s.searchAllIamPoliciesData, s.searchAllIamPoliciesErr
+}
+
+func TestProcessor_UpdatedProcess(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name                string
+		server              *fakeAssetInventoryServer
+		resourceMapping     *v1alpha1.ResourceMapping
+		wantResourceMapping *v1alpha1.ResourceMapping
+		wantErrSubstr       string
+	}{
+		{
+			name: "success",
+			server: &fakeAssetInventoryServer{
+				searchAllResourcesData: &assetpb.SearchAllResourcesResponse{
+					Results: []*assetpb.ResourceSearchResult{{
+						Name:                   "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+						AssetType:              "pubsub.googleapis.com/Topic",
+						Project:                "projects/0",
+						Folders:                []string{"folders/0", "folders/1"},
+						Organization:           "organizations/0",
+						DisplayName:            "projects/test-project/topics/test-topic",
+						Labels:                 map[string]string{"env": "dev"},
+						Location:               "global",
+						ParentAssetType:        "cloudresourcemanager.googleapis.com/Project",
+						ParentFullResourceName: "//cloudresourcemanager.googleapis.com/projects/test-project",
+					}},
+				},
+				searchAllIamPoliciesData: &assetpb.SearchAllIamPoliciesResponse{
+					Results: []*assetpb.IamPolicySearchResult{{
+						Resource:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+						AssetType:    "pubsub.googleapis.com/Topic",
+						Project:      "projects/0",
+						Folders:      []string{"folders/0", "folders/1"},
+						Organization: "organizations/0",
+						//nolint:staticcheck // see import.
+						Policy: &v1.Policy{
+							//nolint:staticcheck // see import.
+							Bindings: []*v1.Binding{{
+								Role:    "roles/pubsub.publisher",
+								Members: []string{"serviceAccount:test-service@gcp-sa-pubsub.iam.gserviceaccount.com"},
+							}},
+						},
+					}},
+				},
+			},
+			resourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantResourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+				Annotations: &structpb.Struct{Fields: map[string]*structpb.Value{
+					"ancestors": structpb.NewListValue(&structpb.ListValue{Values: []*structpb.Value{structpb.NewStringValue("organizations/0"), structpb.NewStringValue("folders/0"), structpb.NewStringValue("folders/1"), structpb.NewStringValue("projects/0")}}),
+					"labels": structpb.NewStructValue(&structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"env": structpb.NewStringValue("dev"),
+						},
+					}),
+					"location": structpb.NewStringValue("global"),
+					"iamPolicies": structpb.NewListValue(&structpb.ListValue{Values: []*structpb.Value{structpb.NewStructValue(&structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"bindings": structpb.NewListValue(&structpb.ListValue{Values: []*structpb.Value{structpb.NewStructValue(&structpb.Struct{
+								Fields: map[string]*structpb.Value{
+									"members": structpb.NewListValue(&structpb.ListValue{Values: []*structpb.Value{structpb.NewStringValue("serviceAccount:test-service@gcp-sa-pubsub.iam.gserviceaccount.com")}}),
+									"role":    structpb.NewStringValue("roles/pubsub.publisher"),
+								},
+							})}}),
+						},
+					})}}),
+				}},
+			},
+		},
+		{
+			name: "failure_with_resources_search_err",
+			server: &fakeAssetInventoryServer{
+				searchAllResourcesErr: fmt.Errorf("encountered error during resources search: Internal Server Error"),
+				searchAllIamPoliciesData: &assetpb.SearchAllIamPoliciesResponse{
+					Results: []*assetpb.IamPolicySearchResult{{
+						Resource:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+						AssetType:    "pubsub.googleapis.com/Topic",
+						Project:      "projects/0",
+						Folders:      []string{"folders/0", "folders/1"},
+						Organization: "organizations/0",
+						//nolint:staticcheck // see import.
+						Policy: &v1.Policy{
+							//nolint:staticcheck // see import.
+							Bindings: []*v1.Binding{{
+								Role:    "roles/pubsub.publisher",
+								Members: []string{"serviceAccount:test-service@gcp-sa-pubsub.iam.gserviceaccount.com"},
+							}},
+						},
+					}},
+				},
+			},
+			resourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantResourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantErrSubstr: "encountered error during resources search: Internal Server Error",
+		},
+		{
+			name: "failure_with_zero_matched_resource",
+			server: &fakeAssetInventoryServer{
+				searchAllResourcesData: &assetpb.SearchAllResourcesResponse{
+					Results: []*assetpb.ResourceSearchResult{},
+				},
+			},
+			resourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantResourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+
+			wantErrSubstr: "0 matched resources found, expected 1 matched resource",
+		},
+		{
+			name: "failure_with_policies_search_err",
+			server: &fakeAssetInventoryServer{
+				searchAllResourcesData: &assetpb.SearchAllResourcesResponse{
+					Results: []*assetpb.ResourceSearchResult{{
+						Name:                   "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+						AssetType:              "pubsub.googleapis.com/Topic",
+						Project:                "projects/0",
+						Folders:                []string{"folders/0", "folders/1"},
+						Organization:           "organizations/0",
+						DisplayName:            "projects/test-project/topics/test-topic",
+						Labels:                 map[string]string{"env": "dev"},
+						Location:               "global",
+						ParentAssetType:        "cloudresourcemanager.googleapis.com/Project",
+						ParentFullResourceName: "//cloudresourcemanager.googleapis.com/projects/test-project",
+					}},
+				},
+				searchAllIamPoliciesErr: fmt.Errorf("encountered error during iam policies search: Internal Server Error"),
+			},
+			resourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantResourceMapping: &v1alpha1.ResourceMapping{
+				Resource: &v1alpha1.Resource{
+					Provider: "gcp",
+					Name:     "//pubsub.googleapis.com/projects/test-project/topics/test-topic",
+				},
+				Contacts: &v1alpha1.Contacts{Email: []string{"pmap.gmail.com"}},
+			},
+			wantErrSubstr: "encountered error during iam policies search: Internal Server Error",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+
+			// Setup fake Asset Inventory server.
+			addr, conn := testutil.FakeGRPCServer(t, func(s *grpc.Server) {
+				assetpb.RegisterAssetServiceServer(s, tc.server)
+			})
+
+			// Setup fake Asset Inventory client.
+			fakeAssetClient, err := asset.NewClient(ctx, option.WithGRPCConn(conn))
+			if err != nil {
+				t.Fatalf("creating client for fake at %q: %v", addr, err)
+			}
+			p, err := NewProcessor(ctx, "projects/fake-project", WithClient(fakeAssetClient))
+			if err != nil {
+				t.Fatalf("failed to create AssetInventoryProcessor: %v", err)
+			}
+
+			// Run test.
+			gotErr := p.Process(ctx, tc.resourceMapping)
+			if diff := testutil.DiffErrString(gotErr, tc.wantErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+			// Verify that the ResourceMapping is modified with additional annotations fetched from Asset Inventory.
+			if diff := cmp.Diff(tc.wantResourceMapping, tc.resourceMapping, protocmp.Transform()); diff != "" {
+				t.Errorf("Process(%+v) got diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
