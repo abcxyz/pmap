3dd1ee932902536f2aa178519646a9473d37ae9e
Suhong Qin <suhongq@google.com>
2023-02-10 22:57:10 +0000

    feat: add pubsub publish implementation for bq

---
+++ pkg/server/pubsub.go
@@ -0,0 +1,93 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+)
+
+// PubSubMessenger implements the Messenger interface for Google Cloud PubSub.
+type PubSubMessenger struct {
+	client *pubsub.Client
+	topic  *pubsub.Topic
+}
+
+// MessengerOption is the option to set up a PubSubMessenger.
+type MessengerOption func(p *PubSubMessenger) (*PubSubMessenger, error)
+
+// WithClient provides a PubSub client to the PubSubMessenger.
+func WithClient(client *pubsub.Client) MessengerOption {
+	return func(p *PubSubMessenger) (*PubSubMessenger, error) {
+		p.client = client
+		return p, nil
+	}
+}
+
+// WithTopic provides a PubSub topic to the PubSubMessenger.
+func WithTopic(topic *pubsub.Topic) MessengerOption {
+	return func(p *PubSubMessenger) (*PubSubMessenger, error) {
+		p.topic = topic
+		return p, nil
+	}
+}
+
+// NewPubSubMessenger creates a new instance of the PubSubMessenger.
+func NewPubSubMessenger(ctx context.Context, projectID, topicID string, opts ...MessengerOption) (*PubSubMessenger, error) {
+	p := &PubSubMessenger{}
+	for _, opt := range opts {
+		var err error
+		p, err = opt(p)
+		if err != nil {
+			return nil, fmt.Errorf("failed to apply messenger options: %w", err)
+		}
+	}
+	if p.client == nil {
+		client, err := pubsub.NewClient(ctx, projectID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create new PubSub client: %w", err)
+		}
+		p.client = client
+	}
+
+	if p.topic == nil {
+		p.topic = p.client.Topic(topicID)
+	}
+
+	return p, nil
+}
+
+// Send sends a message to a Google Cloud PubSub topic.
+func (p *PubSubMessenger) Send(ctx context.Context, msg []byte) error {
+	result := p.topic.Publish(ctx, &pubsub.Message{
+		Data: msg,
+	})
+
+	if _, err := result.Get(ctx); err != nil {
+		return fmt.Errorf("pubsub: failed to get result: %w", err)
+	}
+	return nil
+}
+
+// Cleanup handles the graceful shutdown of the PubSub client.
+func (p *PubSubMessenger) Cleanup() error {
+	p.topic.Stop()
+	if err := p.client.Close(); err != nil {
+		return fmt.Errorf("failed to close PubSub client: %w", err)
+	}
+	return nil
+}
---
+++ pkg/server/pubsub_test.go
@@ -0,0 +1,174 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"context"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub/pstest"
+	"github.com/abcxyz/pkg/testutil"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	serverProjectID      = "test-project-id"
+	serverTopicID        = "test-topic-id"
+	injectedPublishError = "injected publish error"
+)
+
+func TestPubSubMessenger_Send(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	pubSubClient := newTestPubSubClient(ctx, t)
+	pubSubErrClient := newTestPubSubClient(ctx, t, pstest.WithErrorInjection("Publish", codes.NotFound, injectedPublishError))
+
+	cases := []struct {
+		name          string
+		pubSubClient  *pubsub.Client
+		message       []byte
+		wantErrSubstr string
+	}{
+		{
+			name:         "success",
+			pubSubClient: pubSubClient,
+			message:      []byte("test"),
+		},
+		{
+			name:          "error_send_message",
+			pubSubClient:  pubSubErrClient,
+			message:       []byte("test"),
+			wantErrSubstr: injectedPublishError,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			msger, err := NewPubSubMessenger(ctx, serverProjectID, serverTopicID, WithClient(tc.pubSubClient))
+			if err != nil {
+				t.Fatalf("failed to create new PubSubMessenger: %v", err)
+			}
+
+			gotErr := msger.Send(ctx, tc.message)
+			if diff := testutil.DiffErrString(gotErr, tc.wantErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+		})
+	}
+}
+
+func TestPubSubMessenger_Cleanup(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	// Create a PubSub test server that does not close.
+	svr := pstest.NewServer()
+	conn, err := grpc.DialContext(ctx, svr.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("fail to connect to test PubSub server: %v", err)
+	}
+	pubSubClient, err := pubsub.NewClient(ctx, serverProjectID, option.WithGRPCConn(conn))
+	if err != nil {
+		t.Fatalf("failed to create new PubSub test client: %v", err)
+	}
+
+	pubSubClientClose := newTestPubSubClient(ctx, t)
+
+	cases := []struct {
+		name          string
+		pubSubClient  *pubsub.Client
+		wantErrSubstr string
+	}{
+		{
+			name:         "success",
+			pubSubClient: pubSubClient,
+		},
+		{
+			name:          "dup_close",
+			pubSubClient:  pubSubClientClose,
+			wantErrSubstr: "failed to close PubSub client",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			msger, err := NewPubSubMessenger(ctx, serverProjectID, serverTopicID, WithClient(pubSubClient))
+			if err != nil {
+				t.Fatalf("failed to create new PubSubMessenger: %v", err)
+			}
+
+			gotErr := msger.Cleanup()
+			if diff := testutil.DiffErrString(gotErr, tc.wantErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+		})
+	}
+}
+
+// Creates a fake PubSub client.
+func newTestPubSubClient(ctx context.Context, t *testing.T, opts ...pstest.ServerReactorOption) *pubsub.Client {
+	t.Helper()
+
+	// Create PubSub test server.
+	svr := pstest.NewServer(opts...)
+
+	// Connect to the server without using TLS.
+	conn, err := grpc.DialContext(ctx, svr.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("fail to connect to test PubSub server: %v", err)
+	}
+
+	// Create PubSub test client.
+	client, err := pubsub.NewClient(ctx, serverProjectID, option.WithGRPCConn(conn))
+	if err != nil {
+		t.Fatalf("failed to create new PubSub test client: %v", err)
+	}
+
+	// Create the test topic.
+	if _, err := client.CreateTopic(ctx, serverTopicID); err != nil {
+		t.Fatalf("failed to create test PubSub topic: %v", err)
+	}
+
+	t.Cleanup(func() {
+		if err := svr.Close(); err != nil {
+			t.Fatalf("failed to cleanup test PubSub server: %v", err)
+		}
+
+		if err := conn.Close(); err != nil {
+			t.Fatalf("failed to cleanup test PubSub client connection: %v", err)
+		}
+
+		if err := client.Close(); err != nil {
+			t.Fatalf("failed to cleanup test PubSub client: %v", err)
+		}
+	})
+
+	return client
+}
