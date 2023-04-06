6ab361c1024bad58797d11d523e9593e29d3e973
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2023-04-06 14:58:57 -0700

    leverage abcxyz/pkg cli

---
+++ pkg/cli/validate_mapping.go
@@ -0,0 +1,142 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/mail"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/abcxyz/pkg/cli"
+	"github.com/abcxyz/pkg/protoutil"
+	"github.com/abcxyz/pmap/apis/v1alpha1"
+)
+
+var _ cli.Command = (*ValidateMappingCommand)(nil)
+
+type ValidateMappingCommand struct {
+	cli.BaseCommand
+
+	flagPath string
+}
+
+func (c *ValidateMappingCommand) Desc() string {
+	return `Verify the Resource Mapping YAML files that exists in the given path`
+}
+
+func (c *ValidateMappingCommand) Help() string {
+	return `
+Usage: {{ COMMAND }} [options]
+
+  Validate the Resource Mapping YAML files that exists in the given path:
+
+      pmapctl validate-mapping -path "a/b/c"
+`
+}
+
+func (c *ValidateMappingCommand) Flags() *cli.FlagSet {
+	set := cli.NewFlagSet()
+
+	// Command options
+	f := set.NewSection("COMMAND OPTIONS")
+
+	f.StringVar(&cli.StringVar{
+		Name:    "path",
+		Target:  &c.flagPath,
+		Example: "a/b/c",
+		Usage:   `Validate the Resource Mapping YAML files that exists in the given path.`,
+	})
+
+	return set
+}
+
+func (c *ValidateMappingCommand) Run(ctx context.Context, args []string) error {
+	f := c.Flags()
+	if err := f.Parse(args); err != nil {
+		return fmt.Errorf("failed to parse flags: %w", err)
+	}
+	args = f.Args()
+	if len(args) > 0 {
+		return fmt.Errorf("unexpected arguments: %q", args)
+	}
+
+	if c.flagPath == "" {
+		return fmt.Errorf("path is required")
+	}
+
+	dir := c.flagPath
+	files, err := fetchExtractedYAMLFiles(dir)
+	if err != nil {
+		return fmt.Errorf("failed to fetch extracted files in dir %s: %w", dir, err)
+	}
+	var sanityCheckErrs error
+	for _, file := range files {
+		originF := strings.TrimPrefix(file, dir)
+		fmt.Fprintln(c.Stdout(), fmt.Sprintf("processing file %q", originF))
+		data, err := os.ReadFile(file)
+		if err != nil {
+			sanityCheckErrs = errors.Join(sanityCheckErrs, fmt.Errorf("failed to read file from %q, %w", originF, err))
+			continue
+		}
+
+		var resourceMapping v1alpha1.ResourceMapping
+		if err = protoutil.FromYAML(data, &resourceMapping); err != nil {
+			sanityCheckErrs = errors.Join(sanityCheckErrs, fmt.Errorf("file %q failed to pass the validation: failed to unmarshal object yaml to resource mapping: %w", originF, err))
+			continue
+		}
+		for _, e := range resourceMapping.Contacts.Email {
+			if !isValidEmail(e) {
+				sanityCheckErrs = errors.Join(sanityCheckErrs, fmt.Errorf("file %q failed to pass the validation: email %q is not valid", originF, e))
+			}
+		}
+		if resourceMapping.Resource.Provider == "" {
+			sanityCheckErrs = errors.Join(sanityCheckErrs, fmt.Errorf("file %q failed to pass the validation: resource provider %q is not valid", originF, resourceMapping.Resource.Provider))
+		}
+	}
+
+	return sanityCheckErrs
+}
+
+func fetchExtractedYAMLFiles(localDir string) ([]string, error) {
+	var files []string
+	if err := filepath.WalkDir(localDir, func(path string, entry os.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("failed to walking scratch directory %q: %w", path, err)
+		}
+
+		if entry.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".yaml" {
+			files = append(files, path)
+		}
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("failed to walk the directory %s: %w", localDir, err)
+	}
+	return files, nil
+}
+
+func isValidEmail(email string) bool {
+	if email == "" {
+		return false
+	}
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
---
+++ pkg/cli/validate_mapping_test.go
@@ -0,0 +1,170 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/abcxyz/pkg/logging"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewValidateCmd(t *testing.T) {
+	t.Parallel()
+
+	ctx := logging.WithLogger(context.Background(), logging.TestLogger(t))
+	td := t.TempDir()
+
+	cases := []struct {
+		name      string
+		args      []string
+		dir       string
+		fileDatas map[string][]byte
+		expOut    string
+		expErr    string
+	}{
+		{
+			name:   "unexpected_args",
+			args:   []string{"foo"},
+			expErr: `unexpected arguments: ["foo"]`,
+		},
+		{
+			name:   "missing_path",
+			args:   nil,
+			expErr: `path is required`,
+		},
+		{
+			name: "valid_contents",
+			dir:  "dir_valid_contents",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(fmt.Sprintf(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/topics/test-topic
+contacts:
+    email:
+        - pmap@gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`)),
+				"file2.yaml": []byte(fmt.Sprintf(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/subscriptions/test-subsriptions
+contacts:
+    email:
+        - pmap@gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`)),
+			},
+			args:   []string{"-path", filepath.Join(td, "dir_valid_contents")},
+			expOut: "processing file \"/file1.yaml\"\nprocessing file \"/file2.yaml\"",
+		},
+		{
+			name: "invalid_yaml",
+			dir:  "dir_invalid_yaml",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(fmt.Sprintf(`
+		foo
+		`)),
+			},
+			args:   []string{"-path", filepath.Join(td, "dir_invalid_yaml")},
+			expErr: "file \"/file1.yaml\" failed to pass the validation: failed to unmarshal object yaml to resource mapping",
+		},
+		{
+			name: "invalid_email",
+			dir:  "dir_invalid_email",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(fmt.Sprintf(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/topics/test-topic
+contacts:
+    email:
+        - pmap.gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`)),
+			},
+			args:   []string{"-path", filepath.Join(td, "dir_invalid_email")},
+			expErr: "file \"/file1.yaml\" failed to pass the validation: email \"pmap.gmail.com\" is not valid",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.dir != "" && tc.fileDatas != nil {
+				if err := os.MkdirAll(filepath.Join(td, tc.dir), 0o755); err != nil {
+					t.Fatal(err)
+				}
+				for name, data := range tc.fileDatas {
+					testCreateFile(t, filepath.Join(td, tc.dir, name), data)
+				}
+
+			}
+
+			var cmd ValidateMappingCommand
+			_, stdout, _ := cmd.Pipe()
+
+			args := append([]string{}, tc.args...)
+
+			if err := cmd.Run(ctx, args); err != nil {
+				if diff := testutil.DiffErrString(err, tc.expErr); diff != "" {
+					t.Fatal(diff)
+				}
+				if err != nil {
+					return
+				}
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOut), strings.TrimSpace(stdout.String())); diff != "" {
+				t.Errorf("output: diff (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func testCreateFile(t *testing.T, name string, data []byte) {
+	t.Helper()
+	f, err := os.Create(name)
+	if err != nil {
+		t.Fatalf("failed to create file %s: %v", name, err)
+	}
+	if _, err = f.Write(data); err != nil {
+		t.Fatalf("failed to write data to file %s: %v", name, err)
+	}
+	if err = f.Close(); err != nil {
+		t.Fatalf("failed to close file %s: %v", name, err)
+	}
+}
