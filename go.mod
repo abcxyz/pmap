5a59c5fcd8ce0777a46a84765b66569826bdad1c
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2023-04-10 17:37:50 -0700

    feat: added sanity check (#59)
    * added sanity check
    
    * fixed lint
    
    * fixed lint:
    
    * leverage abcxyz/pkg cli
    
    * fixed go module
    
    * fixed nits
    
    * fixed lint
    
    * changed cmd to validate
    
    * fixed feedback, moved validation logic to v1alpha1
    
    * removed unused file
    
    * fixed nits
    
    * fixed nits
    
    * fixed test
    
    * fixed path seperator
    
    * updated dependency
    
    * fixed lint

--- go.mod
+++ go.mod
@@ -3,22 +3,23 @@ module github.com/abcxyz/pmap
 go 1.20
 
 require (
-	cloud.google.com/go/asset v1.11.1
-	cloud.google.com/go/bigquery v1.48.0
-	cloud.google.com/go/pubsub v1.28.0
-	github.com/abcxyz/pkg v0.2.1-0.20230317002818-607c6a5a1ae2
+	cloud.google.com/go/asset v1.12.0
+	cloud.google.com/go/bigquery v1.49.0
+	cloud.google.com/go/pubsub v1.30.0
+	github.com/abcxyz/pkg v0.3.1-0.20230410214104-4b190bd00925
 	github.com/google/go-cmp v0.5.9
 	github.com/sethvargo/go-envconfig v0.9.0
 	github.com/sethvargo/go-retry v0.2.4
-	google.golang.org/api v0.110.0
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4
-	google.golang.org/grpc v1.53.0
+	google.golang.org/api v0.114.0
+	google.golang.org/genproto v0.0.0-20230323172734-21a4fbf068fa
+	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	cloud.google.com/go/orgpolicy v1.10.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/apache/arrow/go/v10 v10.0.1 // indirect
+	github.com/apache/arrow/go/v11 v11.0.0 // indirect
 	github.com/apache/thrift v0.16.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v2.0.8+incompatible // indirect
@@ -27,40 +28,42 @@ require (
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
 	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.uber.org/goleak v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/mod v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
 	cloud.google.com/go v0.110.0 // indirect
-	cloud.google.com/go/accesscontextmanager v1.6.0 // indirect
-	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/accesscontextmanager v1.7.0 // indirect
+	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.12.0 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
 	cloud.google.com/go/longrunning v0.4.1 // indirect
 	cloud.google.com/go/osconfig v1.11.0 // indirect
 	cloud.google.com/go/storage v1.29.0
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
-	github.com/goccy/go-json v0.10.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
-	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
-	github.com/lestrrat-go/jwx/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.9 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
@@ -68,7 +71,7 @@ require (
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
---
+++ pkg/cli/validate_test.go
@@ -0,0 +1,164 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/abcxyz/pkg/logging"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewValidateCmd(t *testing.T) {
+	t.Parallel()
+
+	ctx := logging.WithLogger(context.Background(), logging.TestLogger(t))
+	td := t.TempDir()
+
+	cases := []struct {
+		name      string
+		args      []string
+		dir       string
+		fileDatas map[string][]byte
+		expOut    string
+		expErr    string
+	}{
+		{
+			name:   "unexpected_args",
+			args:   []string{"foo"},
+			expErr: `unexpected arguments: [foo]`,
+		},
+		{
+			name:   "missing_type",
+			args:   []string{"-path", filepath.Join(td, "dir_missing_type")},
+			expErr: `unsupported type`,
+		},
+		{
+			name:   "missing_path",
+			args:   []string{"-type", "ResourceMapping"},
+			expErr: `path is required`,
+		},
+		{
+			name: "valid_contents",
+			dir:  "dir_valid_contents",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/topics/test-topic
+contacts:
+    email:
+        - pmap@gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`),
+				"file2.yaml": []byte(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/subscriptions/test-subsriptions
+contacts:
+    email:
+        - pmap@gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`),
+			},
+			args:   []string{"-type", "ResourceMapping", "-path", filepath.Join(td, "dir_valid_contents")},
+			expOut: "processing file \"file1.yaml\"\nprocessing file \"file2.yaml\"\nValidation passed",
+		},
+		{
+			name: "invalid_yaml",
+			dir:  "dir_invalid_yaml",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(`
+		foo
+		`),
+			},
+			args:   []string{"-type", "ResourceMapping", "-path", filepath.Join(td, "dir_invalid_yaml")},
+			expErr: "file \"file1.yaml\": failed to unmarshal yaml to ResourceMapping",
+		},
+		{
+			name: "invalid_email",
+			dir:  "dir_invalid_email",
+			fileDatas: map[string][]byte{
+				"file1.yaml": []byte(`
+resource:
+    provider: gcp
+    name: //pubsub.googleapis.com/projects/test-project/topics/test-topic
+contacts:
+    email:
+        - pmap.gmail.com
+annotations:
+    fields:
+        location:
+            kind:
+                stringvalue: global
+`),
+			},
+			args:   []string{"-type", "ResourceMapping", "-path", filepath.Join(td, "dir_invalid_email")},
+			expErr: "file \"file1.yaml\": invalid owner",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.dir != "" && tc.fileDatas != nil {
+				if err := os.MkdirAll(filepath.Join(td, tc.dir), 0o755); err != nil {
+					t.Fatal(err)
+				}
+				for name, data := range tc.fileDatas {
+					if err := os.WriteFile(filepath.Join(td, tc.dir, name), data, 0o600); err != nil {
+						t.Fatalf("failed to write data to file %s: %v", name, err)
+					}
+				}
+			}
+
+			var cmd ValidateCommand
+			_, stdout, _ := cmd.Pipe()
+
+			args := append([]string{}, tc.args...)
+
+			if err := cmd.Run(ctx, args); err != nil {
+				if diff := testutil.DiffErrString(err, tc.expErr); diff != "" {
+					t.Fatal(diff)
+				}
+				if err != nil {
+					return
+				}
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOut), strings.TrimSpace(stdout.String())); diff != "" {
+				t.Errorf("output: diff (-want, +got):\n%s", diff)
+			}
+			if want, got := strings.TrimSpace(tc.expOut), strings.TrimSpace(stdout.String()); got != want {
+				t.Errorf("got\n\n%s\n\nwant\n\n%s\n\n", got, want)
+			}
+		})
+	}
+}
