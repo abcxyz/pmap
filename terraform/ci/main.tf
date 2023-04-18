cd25f87f8481f7498b1c59dd92064c22d3ab97c8
Qinhang Li <55097418+sailorlqh@users.noreply.github.com>
2023-04-18 15:30:17 -0700

    add static bucket for integ test (#67)
    * add static bucket for integ test
    
    * move static_bucket to ci folder
    
    * add static gcs bucket name to output
    
    * add label and default name for static bucket

--- terraform/ci/main.tf
+++ terraform/ci/main.tf
@@ -19,6 +19,39 @@ module "common_infra" {
 
   gcs_bucket_name = var.gcs_bucket_name
   event_types     = var.event_types
+
   // Terraform destroy or terraform apply that would delete the table instance will fail.
   bigquery_table_delete_protection = true
+}
+
+resource "random_id" "default" {
+  byte_length = 2
+}
+
+
+locals {
+  static_bucket_name = "pmap-static-ci-bucket-${random_id.default.hex}"
+}
+
+resource "google_storage_bucket" "integ_test_dedicated_bucket" {
+  project = var.project_id
+
+  name                        = local.static_bucket_name
+  location                    = "US"
+  force_destroy               = false
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  labels = {
+    env = "pmap-ci-test"
+  }
+}
+
+# Add service account to static bucket IAM
+# for testing purpose, therefore the IAM policy
+# list won't be empty
+resource "google_storage_bucket_iam_member" "static_bucket_object_viewer" {
+  bucket = google_storage_bucket.integ_test_dedicated_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = module.common_infra.run_service_account.member
 }
