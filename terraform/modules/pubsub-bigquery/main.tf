e1644733e2a0e83ea16b4d059fb65a9a657f2f0f
Suhong Qin <51539171+sqin2019@users.noreply.github.com>
2023-06-14 13:46:53 -0700

    feat: add dlq for bigquery subscription (#121)
    fix: add dlq for bigquery subscription

--- terraform/modules/pubsub-bigquery/main.tf
+++ terraform/modules/pubsub-bigquery/main.tf
@@ -13,9 +13,14 @@
 // limitations under the License.
 
 locals {
-  success_table = var.event
-  failure_table = "${var.event}-failure"
-  tables        = toset([local.success_table, local.failure_table])
+  success_table            = var.event
+  failure_table            = "${var.event}-failure"
+  tables                   = toset([local.success_table, local.failure_table])
+  pubsub_svc_account_email = "service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+data "google_project" "project" {
+  project_id = var.project_id
 }
 
 resource "google_project_service" "serviceusage" {
@@ -115,13 +120,63 @@ resource "google_pubsub_subscription" "bigquery" {
     table          = "${var.project_id}.${var.dataset_id}.${google_bigquery_table.pmap[each.key].table_id}"
     write_metadata = true
   }
+
+  dead_letter_policy {
+    dead_letter_topic     = google_pubsub_topic.bigquery_dead_letter[each.key].id
+    max_delivery_attempts = 7
+  }
 }
 
-// Grant Pub/Sub publisher role of Pub/Sub topics to the pmap service account.
+resource "google_pubsub_topic" "bigquery_dead_letter" {
+  for_each = local.tables
+
+  project = var.project_id
+
+  name = "${each.key}-bigquery-dead-letter"
+
+  depends_on = [
+    google_project_service.services["pubsub.googleapis.com"]
+  ]
+}
+
+# A dummy subscription is required as the dead letter topic should have at
+# least one subscription so that dead-lettered messages will not be lost.
+resource "google_pubsub_subscription" "bigquery_dead_letter" {
+  for_each = local.tables
+
+  project = var.project_id
+
+  name  = "${each.key}-bigquery-dead-letter"
+  topic = google_pubsub_topic.bigquery_dead_letter[each.key].id
+}
+
+# Grant Pub/Sub publisher role of Pub/Sub topics to the pmap service account.
 resource "google_pubsub_topic_iam_member" "publisher" {
   for_each = local.tables
 
   topic  = google_pubsub_topic.bigquery[each.key].id
   role   = "roles/pubsub.publisher"
   member = "serviceAccount:${var.run_service_account}"
+}
+
+# The Cloud Pub/Sub service account for this project needs the publisher role to
+# publish dead-lettered messages to the dead letter topic.
+resource "google_pubsub_topic_iam_member" "dead_letter_publisher" {
+  for_each = local.tables
+
+  topic  = google_pubsub_topic.bigquery_dead_letter[each.key].id
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${local.pubsub_svc_account_email}"
+}
+
+# The Cloud Pub/Sub service account for this project needs the subscriber role
+# to forward messages from this subscription to the dead letter topic.
+resource "google_pubsub_subscription_iam_member" "dead_letter_subscriber" {
+  for_each = local.tables
+
+  project = var.project_id
+
+  subscription = google_pubsub_subscription.bigquery[each.key].id
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${local.pubsub_svc_account_email}"
 }
