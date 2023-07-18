44f4fe1eb4a60f3e268dbeff0ae9ec085e3a6299
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2023-07-18 14:27:34 -0700

    fixed terraform lint

--- terraform/modules/pmap-service/main.tf
+++ terraform/modules/pmap-service/main.tf
@@ -73,15 +73,15 @@ resource "google_pubsub_subscription" "pmap" {
 }
 
 resource "google_pubsub_topic" "gcs_dead_letter" {
-  count = var.enable_dead_lettering ? 1 : 0
+  count   = var.enable_dead_lettering ? 1 : 0
   project = var.project_id
-  name  = "${module.service.service_name}-gcs-dead-letter"
+  name    = "${module.service.service_name}-gcs-dead-letter"
 }
 
 # A dummy subscription is required as the dead letter topic should have at
 # least one subscription so that dead-lettered messages will not be lost.
 resource "google_pubsub_subscription" "gcs_dead_letter" {
-  count = var.enable_dead_lettering ? 1 : 0
+  count   = var.enable_dead_lettering ? 1 : 0
   project = var.project_id
 
   name  = "${module.service.service_name}-gcs-dead-letter"
@@ -95,7 +95,7 @@ resource "google_pubsub_subscription" "gcs_dead_letter" {
 # The Cloud Pub/Sub service account for this project needs the publisher role to
 # publish dead-lettered messages to the dead letter topic.
 resource "google_pubsub_topic_iam_member" "dead_letter_publisher" {
-  count = var.enable_dead_lettering ? 1 : 0
+  count   = var.enable_dead_lettering ? 1 : 0
   project = var.project_id
 
   topic  = google_pubsub_topic.gcs_dead_letter[0].id
@@ -106,7 +106,7 @@ resource "google_pubsub_topic_iam_member" "dead_letter_publisher" {
 # The Cloud Pub/Sub service account for this project needs the subscriber role
 # to forward messages from this subscription to the dead letter topic.
 resource "google_pubsub_subscription_iam_member" "dead_letter_subscriber" {
-  count = var.enable_dead_lettering ? 1 : 0
+  count   = var.enable_dead_lettering ? 1 : 0
   project = var.project_id
 
   subscription = google_pubsub_subscription.pmap.id
